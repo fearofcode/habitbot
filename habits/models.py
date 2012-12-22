89bbfa2a6b6b784144a31cb5d0d0fe06bc4eb2be
Warren Henning <warren.henning@gmail.com>
2012-12-22 09:12:41 -0800

    More date parsing

--- habits/models.py
+++ habits/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+import datetime
 from recurrent import RecurringEvent
+
+class InvalidInput(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 class Goal(models.Model):
     EVERY = "every"
@@ -8,17 +16,35 @@ class Goal(models.Model):
     created_at = models.DateField(auto_now_add=True)
 
     creation_text = models.CharField(max_length=200) # for debugging in case i fuck this up
-
     description = models.CharField(max_length=50)
-    recurring = models.BooleanField(default=True)
-    rrule = models.CharField(max_length=200, null=True, blank=True)
+    rrule = models.CharField(max_length=200)
     dtstart = models.DateField()
-    frequency = models.CharField(max_length=50, null=True, blank=True)
+    frequency = models.CharField(max_length=50)
+    interval = models.IntegerField()
 
     def parse(self, goal_text):
+        self.creation_text = goal_text
+
         index = goal_text.find(self.EVERY)
 
+        if index == -1:
+            raise InvalidInput("Could not find the word '%s' in input" % self.EVERY)
+
         self.description = goal_text[:index].strip()
+
+        recurring_event = RecurringEvent()
+        result = recurring_event.parse(self.creation_text)
+
+        if type(result) != type('str'):
+            raise InvalidInput("Not a recurring rule or not valid input")
+
+        self.rrule = recurring_event.get_RFC_rrule()
+
+        params = recurring_event.get_params()
+
+        self.dtstart = params['dtstart'] if params.has_key('dtstart') else datetime.date.today()
+        self.frequency = params['freq']
+        self.interval = params['interval']
 
     def __unicode__(self):
         return self.creation_text
--- habits/tests.py
+++ habits/tests.py
@@ -6,10 +6,11 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from habits.models import Goal
+from habits.models import Goal, InvalidInput
+import datetime
 
 class GoalTest(TestCase):
-    def test_parsing(self):
+    def test_parse_goal(self):
         """
         Tests that we can parse a goal from text input.
         """
@@ -17,4 +18,20 @@ class GoalTest(TestCase):
 
         goal = Goal()
         goal.parse(goal_text)
+        self.assertEqual(goal.creation_text, "Go for a walk every day")
         self.assertEqual(goal.description, "Go for a walk")
+        self.assertEqual(goal.rrule, 'RRULE:FREQ=DAILY;INTERVAL=1')
+        self.assertEqual(goal.dtstart, datetime.date.today())
+        self.assertEqual(goal.frequency, "daily")
+        self.assertEqual(goal.interval, 1)
+
+    def test_complain_invalid_input(self):
+        """
+        Tests that parsing complains on invalid input.
+        """
+
+        bad_input = "Herp a derp all day"
+
+        goal = Goal()
+
+        self.assertRaises(InvalidInput, goal.parse, bad_input)

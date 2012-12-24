86d7df63c0a2f0dcce123b6a89accae98d769181
Warren Henning <warren.henning@gmail.com>
2012-12-24 09:49:31 -0800

    Remove tests for removed code

--- habits/tests.py
+++ habits/tests.py
@@ -6,7 +6,7 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
-from habits.models import Goal, InvalidInput, Instance
+from habits.models import Goal, InvalidInput
 import datetime
 
 class GoalTest(TestCase):
@@ -46,58 +46,6 @@ class GoalTest(TestCase):
         self.assertEqual(self.byday_goal.description, "Go to the gym")
         self.assertEqual(self.byday_goal.rrule, 'DTSTART:20130107\nRRULE:BYDAY=MO,WE,FR;INTERVAL=1;FREQ=WEEKLY')
         self.assertEqual(self.byday_goal.dtstart, datetime.date(2013, 1, 7))
-
-    def test_get_next_datetime(self):
-        tomorrow = self.today + datetime.timedelta(days=1)
-
-        self.assertEqual(self.simple_goal.next_date(), self.today)
-
-        self.assertEqual(self.byday_goal.next_date(), datetime.date(2013, 1, 7))
-
-        self.simple_goal.instance_set.create(created_at=self.today)
-        instance = self.simple_goal.instance_set.all()[0]
-
-        self.assertEqual(instance.compute_due_at(), tomorrow)
-
-        self.assertEqual(self.simple_goal.next_date(), tomorrow)
-
-        self.byday_goal.instance_set.create(created_at=datetime.date(2013, 1, 7))
-        self.assertEqual(self.byday_goal.next_date(), datetime.date(2013, 1, 9))
-
-    def test_next_date(self):
-        """
-        A goal's next date is always at least today, regardless of what completions exist.
-        """
-
-        simple_goal_text = "Go for a walk every day"
-
-        two_days_ago = self.today - datetime.timedelta(days=2)
-        two_days_str = two_days_ago.strftime("%Y%m%d")
-        g = Goal()
-        g.creation_text = "foo"
-        g.description = "foo"
-        g.rrule = "rrule=DTSTART:" + two_days_str + "\nRRULE:FREQ=DAILY;INTERVAL=1"
-        g.dtstart = two_days_ago
-
-        self.assertEqual(g.next_date(), self.today)
-
-    def test_weekly_goals(self):
-        goal_text = "Do a timed mile run every other week"
-
-        goal = Goal()
-        goal.parse(goal_text)
-        goal.save()
-
-        self.assertEqual(goal.creation_text, "Do a timed mile run every other week")
-        self.assertEqual(goal.description, "Do a timed mile run")
-        self.assertEqual(goal.rrule,
-            'DTSTART:' + self.today.strftime("%Y%m%d") + '\nRRULE:FREQ=WEEKLY;INTERVAL=2')
-        self.assertEqual(goal.dtstart, datetime.date.today())
-
-        goal.instance_set.create(created_at=self.today)
-        instance = goal.instance_set.all()[0]
-
-        self.assertEqual(instance.compute_due_at(), self.today + datetime.timedelta(weeks=2))
 
     def test_complain_invalid_input(self):
         """

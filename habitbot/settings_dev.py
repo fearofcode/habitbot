65b11136ad74dcb9853ed8d0418f98224495801d
Warren Henning <warren.henning@gmail.com>
2013-01-14 12:35:15 -0800

    Add settings file that should be free of secret stuff

--- habitbot/settings_dev.py
+++ habitbot/settings_dev.py
@@ -1,10 +1,16 @@
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+
+PROJECT_DIR = os.path.dirname(__file__)
+
+DB_PATH = os.path.join(PROJECT_DIR, "db/habitbot.db")
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/warren/code/habitbot/db/habitbot.db',                      # Or path to database file if using sqlite3.
+        'NAME': DB_PATH,                       # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
